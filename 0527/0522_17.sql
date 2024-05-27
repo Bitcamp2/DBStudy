e45f931e67bf8630e253e48cabf68951b44fc2c9
watashijxxnsuka <jxxn2538@gmail.com>
2024-05-27 20:43:55 +0900

    24.05.27 END

--- 0527/0522_17.sql
+++ 0527/0522_17.sql
@@ -1,6 +1,6 @@
 --1) 각 학과별 학생 수를 검색하세요
 SELECT MAJOR
-	 , COUNT(SNO)
+	 , COUNT(*)
 	FROM STUDENT
 	GROUP BY MAJOR;
 
@@ -18,12 +18,25 @@ SELECT *
 		 		, '생물' AS "생물"
 		 				   )
 		 	   );
+		 	  
+SELECT AVG(AVR*1.125)
+	 , MAJOR
+	FROM STUDENT
+	WHERE MAJOR IN ('화학', '생물')
+	GROUP BY MAJOR;		 	  
+		 	  
 
 --3) 부임일이 10년 이상 된 직급별(정교수, 조교수, 부교수) 교수의 수를 검색하세요
 SELECT ORDERS
 	 , COUNT(*) 
 	FROM PROFESSOR
 	WHERE HIREDATE <= ADD_MONTHS(HIREDATE, +120) 
+	GROUP BY ORDERS;
+
+SELECT COUNT(*)
+	 , ORDERS
+	FROM PROFESSOR
+	WHERE ADD_MONTHS(HIREDATE, 120) <= SYSDATE
 	GROUP BY ORDERS;
 
 --4) 과목명에 화학이 포함된 과목의 학점수 총합을 검색하세요
@@ -41,51 +54,20 @@ SELECT MAJOR
 	GROUP BY MAJOR
 	ORDER BY AVG(RESULT) DESC;
 
---6) 30번 부서의 업무별 연봉의 평균을 검색하세요(소수점 두자리까지 표시)
-SELECT *
-	FROM (
-		SELECT E.DNO
-		     , E.SAL
-		  FROM EMP E
-		  JOIN DEPT D
-		    ON E.DNO = D.DNO
-		 )
-		 PIVOT (
-		 		AVG(SAL) 
-		 		FOR DNO IN (
-		 		  '30' AS "30"
-		 				   )
-		 	   );
+--6) 30번 부서의 업무별 연봉의 평균을 검색하세요(소수점 두자리까지 표시)	  
+SELECT ROUND(AVG(SAL), 2)
+	 , JOB
+	FROM EMP
+	WHERE DNO = 30
+	GROUP BY JOB
+	ORDER BY ROUND(AVG(SAL), 2);
+
 
 --7) 물리학과 학생 중에 학년별로 성적이 가장 우수한 학생의 평점을 검색하세요(학과, 학년, 평점)
-SELECT *
-	FROM(
-			SELECT MAJOR
-				 , SYEAR
-				 , AVR
-				 , AVG(RESULT) AS AVG_RESULT
-				FROM STUDENT S
-				JOIN SCORE SC
-				  ON S.SNO = SC.SNO
-				GROUP BY MAJOR, SYEAR, AVR
-		)
-		 PIVOT(
-		 	MAX(AVG_RESULT)
-		 	 FOR MAJOR IN (
-		 	 			'물리' AS "물리"
-		 	 			  )
-		 	  );
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+SELECT SYEAR
+	 , MAX(AVR)
+	FROM STUDENT 
+	WHERE MAJOR = '물리'
+	GROUP BY SYEAR 
+	ORDER BY SYEAR;
 	
--- 0527/0522_18.sql
+++ 0527/0522_18.sql
@@ -26,7 +26,7 @@ SELECT S.SNAME
 SELECT C.CNO
 	 , C.CNAME
 	 , C.ST_NUM 
-	 , C.PNO
+	 , P.PNO
 	 , P.PNAME
     FROM COURSE c 
     JOIN PROFESSOR p 
@@ -34,8 +34,7 @@ SELECT C.CNO
     WHERE C.ST_NUM >= 3;
 
 --5) 기말고사 평균 성적이 핵 화학과목보다 우수한 과목의 과목명과 담당 교수명을 검색하세요
-SELECT AVG(S.RESULT)
-	 , C.CNAME
+SELECT C.CNAME
 	 , P.PNAME
 	FROM SCORE s 
 	JOIN COURSE c 
@@ -43,22 +42,38 @@ SELECT AVG(S.RESULT)
 	JOIN PROFESSOR p 
 	  ON C.PNO = P.PNO
 	GROUP BY C.CNAME, P.PNAME
-	HAVING AVG(S."RESULT") > AVG(S."RESULT") = '핵화학';
+	HAVING AVG(S."RESULT") > (
+				SELECT AVG(SS.RESULT) 
+					FROM SCORE SS
+					JOIN COURSE CC
+					  ON SS.CNO = CC.CNO
+					WHERE CC.CNAME = '핵화학'
+		);
 	
 
 --6) 근무 중인 직원이 4명 이상인 부서를 검색하세요(부서번호, 부서명, 인원수)
-
+SELECT D.DNO
+	 , D.DNAME
+	 , COUNT(*)
+	FROM DEPT d
+	JOIN EMP e 
+	  ON D.DNO = E.DNO 
+	GROUP BY D.DNO, D.DNAME 
+	HAVING COUNT(*) >= 4;
 
 --7) 업무별 평균 연봉이 3000 이상인 업무를 검색하세요
-
+SELECT JOB
+	FROM EMP e
+	GROUP BY JOB
+	HAVING AVG(SAL) >= 3000; 
 
 --8) 각 학과의 학년별 인원중 인원이 5명 이상인 학년을 검색하세요
 SELECT MAJOR
 	 , SYEAR
 	 , COUNT(*) 
 	FROM STUDENT 
-	GROUP BY MAJOR, SYEAR
-	HAVING COUNT(SNO) >= 5
+	GROUP BY SYEAR, MAJOR
+	HAVING COUNT(*) >= 5
 	ORDER BY MAJOR;
 
 
